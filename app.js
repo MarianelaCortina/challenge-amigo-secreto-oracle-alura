f053687f44dc883e60a78802027a9677e1b9473c
Marianela Cortina <73797352+MarianelaCortina@users.noreply.github.com>
2025-01-27 18:05:13 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+/* El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+Aquí deberás desarrollar la lógica para resolver el problema. */
+
+//Tarea 1: Crear un array para almacenar los nombres de amigos
+
+//let listaAmigos = [];
