5f77fc651ddfd3d1f9bcee2d08e8f885bc6ac2d6
Marianela Cortina <marianelacort@hotmail.com>
2025-01-28 16:28:21 -0300

    Creacion funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 /* El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
 Aquí deberás desarrollar la lógica para resolver el problema. */
 
-//Tarea 1: Crear un array para almacenar los nombres de amigos
-
 let listaAmigos = [];
+let nombreAmigo = "";
+
+function ingresarNombreAmigo(){
+    nombreAmigo = document.getElementById("amigo").value;
+}
+
+function limpiarInput(){
+    document.getElementById("amigo").value = "";
+}
+
+/* function mostrarListaAmigos(){
+    document.getElementById("listaAmigos").innerHTML = "";
+    for(let i = 0; i < listaAmigos.length; i++){
+        document.getElementById("listaAmigos").innerHTML += "<li>" + listaAmigos[i] + "</li>";
+    }
+} */
+
+function agregarAmigo(){
+    ingresarNombreAmigo();
+    if(nombreAmigo === ""){
+        alert("Por favor inserte un nombre");
+        return;
+    }
+    else if(listaAmigos.includes(nombreAmigo)){
+        alert("Este amigo ya fue agregado");
+        limpiarInput();
+        return;
+    }else{
+        listaAmigos.push(nombreAmigo);
+        //console.log(listaAmigos);
+        //mostrarListaAmigos();
+    }
+    limpiarInput();
+}
