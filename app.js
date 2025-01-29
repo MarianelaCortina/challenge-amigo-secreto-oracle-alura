e307bb4b9f3d205882533f795e3de786eee7ed30
Marianela Cortina <marianelacort@hotmail.com>
2025-01-29 15:30:34 -0300

    Creacion funcion sortear amigos

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let nombreAmigo = "";
 function ingresarNombreAmigo(){
     nombreAmigo = document.getElementById("amigo").value;
 }
-
+//mejorar esta funcion, para que reciba el id como parametro y pueda ser reutilizable
 function limpiarInput(){
     document.getElementById("amigo").value = "";
 }
@@ -17,7 +17,19 @@ function mostrarListaAmigos(){
     for(let i = 0; i < listaAmigos.length; i++){
         document.getElementById("listaAmigos").innerHTML += "<li>" + listaAmigos[i] + "</li>";
     }
+} 
+
+function limpiarListaAmigos(){
+    document.getElementById("listaAmigos").innerHTML = "";
+    //listaAmigos = [''];
 }
+
+function asignarTextoElemento(idElemento, texto){
+    document.getElementById(idElemento).innerHTML = texto;
+}
+
+
+
 
 function agregarAmigo(){
     ingresarNombreAmigo();
@@ -29,10 +41,30 @@ function agregarAmigo(){
         alert("Este amigo ya fue agregado");
         limpiarInput();
         return;
-    }else{
+    } 
+    else {
         listaAmigos.push(nombreAmigo);
-        //console.log(listaAmigos);
         mostrarListaAmigos();
     }
     limpiarInput();
 }
+
+
+function generarAmigoSorteado(listaAmigos){
+    let amigoSorteado = Math.floor(Math.random() * listaAmigos.length);
+    console.log
+    return listaAmigos[amigoSorteado];
+}
+
+function sortearAmigo(){
+    if(listaAmigos == ''){
+        alert("Por favor proporcione al menos un amigo");
+        return;
+    }
+    else {
+        let amigoGanador = generarAmigoSorteado(listaAmigos);
+        document.getElementById("resultado").innerHTML += "<li>" + `El amigo secreto sorteado es:  ${amigoGanador}` + "</li>";
+        limpiarListaAmigos();
+    }
+}
+
